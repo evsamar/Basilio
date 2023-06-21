b896fd7ddabc3b6fa8d750d25817dea93c19f57d
Samarkin <siavmaanr@gmail.com>
2023-06-21 23:32:36 +0300

    fix: fix services scroll

--- pages/services/scripts/services.js
+++ pages/services/scripts/services.js
@@ -1,17 +1,24 @@
-const sliderLine = document.querySelector('.slider-line'),
-    prewButton = document.querySelector('.prev-btn'),
-    nextButton = document.querySelector('.next-btn'),
-    dots = document.querySelectorAll('.dots');
+const sliderLine = document.querySelector('.slider-line')
+const prevButton = document.querySelector('.prev-btn')
+const nextButton = document.querySelector('.next-btn')
+//тут нужно было получить не dots, а dot ==================================== !!!
+const dots = document.querySelectorAll('.dot')
 
 let position = 0;
-dotIndex = 0;
+let dotIndex = 0;
 
-
-//FUNCTIONS
+//тут тут вынес константу, а dot ==================================== !!!
+const serviceWidth = 1275
 
 const nextSlide = () => {
-    if (position < (dots.length - 1) * 1275) {
-        position += 1275;
+    //можно сделать так
+    const condition = (dots.length - 1) * serviceWidth
+    position = condition ? position + serviceWidth : 0
+    position = condition ? position + 1 : 0
+
+    //вместо этого
+    if (position < (dots.length - 1) * serviceWidth) {
+        position += serviceWidth;
         dotIndex++;
     } else {
         position = 0;
@@ -24,10 +31,10 @@ const nextSlide = () => {
 
 const prevSlide = () => {
     if (position > 0) {
-        position -= 1275;
+        position -= serviceWidth;
         dotIndex--;
     } else {
-        position = (dots.length - 1) * 1275;
+        position = (dots.length - 1) * serviceWidth;
         dotIndex = (dots.length - 1);
     }
 
@@ -39,11 +46,8 @@ const thisSlide = (index) => {
     for (let dot of dots) {
         dot.classList.remove('active');
     }
-    dots(index).classList.add('active');
+    dots[index].classList.add('active');
 }
-
-
-//EVENTLISTENERS
 
 
 nextButton.addEventListener('click', nextSlide);
@@ -51,15 +55,13 @@ prevButton.addEventListener('click', prevSlide);
 
 dots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
-        position = 1275 * index
+        position = serviceWidth * index
         sliderLine.style.left = -position + 'px'
         dotIndex = index
         thisSlide(dotIndex)
     })
 })
 
-
-//SETINTERVAL
 
 setInterval(() => {
     nextSlide()
